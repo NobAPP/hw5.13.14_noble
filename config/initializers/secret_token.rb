976d0d4243fd676c3b5b82ab62c13a14e0441e3b
zkirchin <zacharykirchin@gmail.com>
2014-05-13 11:09:38 -0400

    all things we did in class

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Class51314::Application.config.secret_key_base = 'd19ebf65c4b8d226a82b3cc05d2ded394ce256b24ce3a99bd407eaeee2ee1628011c57fe01c1cd95bd067b8d3a477c2f16cf106d77293b1372318319056b98b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Class51314::Application.config.session_store :cookie_store, key: '_class_5_13_14_session'
